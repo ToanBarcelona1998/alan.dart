f2f3b1ecdb0bd7be72eb81f9f2007012cb12de22
ToanBarcelona1998 <77067868+ToanBarcelona1998@users.noreply.github.com>
2025-07-09 15:05:09 +0700

    feat: update old library

--- lib/utils/bip_39.dart
+++ lib/utils/bip_39.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
-
-import 'package:bip39/bip39.dart' as bip39;
+import 'package:bip39_mnemonic/bip39_mnemonic.dart';
 
 /// Wrapper around the Bip-39 library making it easier to deal with
 /// mnemonic phrases.
@@ -9,17 +8,22 @@ class Bip39 {
 
   /// Returns [true] if the provided mnemonic is valid, or [false] otherwise.
   static bool validateMnemonic(List<String> mnemonic) {
-    return bip39.validateMnemonic(mnemonic.join(_separator));
+    try{
+      Mnemonic.fromWords(words: mnemonic).entropy;
+      return true;
+    }catch (e){
+      return false;
+    }
   }
 
   /// Returns a seed from the provided mnemonic that can be used to generate
   /// a new wallet.
   static Uint8List mnemonicToSeed(List<String> mnemonic) {
-    return bip39.mnemonicToSeed(mnemonic.join(_separator));
+    return Uint8List.fromList(Mnemonic.fromWords(words: mnemonic).seed);
   }
 
   /// Returns a randomly generated seed phrase.
   static List<String> generateMnemonic({int strength = 128}) {
-    return bip39.generateMnemonic(strength: strength).split(_separator);
+    return Mnemonic.generate(Language.english , entropyLength:  strength).words;
   }
 }
